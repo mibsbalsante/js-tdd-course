9d219e79087b27a8c3f4db19db963fcb52354c01
Mibs Balsante <mibsbalsante@ckl.io>
2018-06-21 16:40:06 -0300

    Update example

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const TOKEN_API = '';
+const TOKEN_API = 'BQDaW1ibwKSrfh_0OFfYnqZXOtAQCCQH8zfD7mwWCk2vNNRcgAh1zWNo2IUG30wk-USUec5imqHC_zW_eoSyFM9HbWAVkPcrHBsiaIhv3KmIruwds_3XuWaMaZQkXn3uJrVweIw4x6drowQ';
 
 export const API_URL = 'https://api.spotify.com/v1';
